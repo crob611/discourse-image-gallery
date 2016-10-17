fc4dafefa95ca99f57f5bbdf74e2a6928d0adc3b
chasovoy85 <tis-nn@ya.ru>
2016-10-17 13:49:12 +0300

    Update image_gallery_dialect.js

--- assets/javascripts/image_gallery_dialect.js
+++ assets/javascripts/image_gallery_dialect.js
@@ -1,9 +1,9 @@
 (function() {
   function replaceImageGalery (text) {
-    text.replace("[image-gallery]","<p><div class='discourse-image-gallery;'>");
-    text.replace("[/image-gallery]","</div></p>");
+    text = text.replace("[image-gallery]","<p><div class='discourse-image-gallery;'>");
+    text = text.replace("[/image-gallery]","</div></p>");
     return text;
-  }
+}
 
   Discourse.Dialect.addPreProcessor(replaceImageGalery);
 })();
